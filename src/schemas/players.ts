a57cc9330922e928581ae0e1baad2e06bffe6a3e
Noé Henchoz <henchoznoe@gmail.com>
2025-01-16 13:21:59 +0100

    Bugs fix

--- src/schemas/players.ts
+++ src/schemas/players.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const addPlayerSchema = z.object({
-  username: z.string().regex(/^[a-zA-Z0-9._-]{1,32}$/, 'Le nom d\'utilisateur doit contenir entre 1 et 32 caractères'),
-  riot_username: z.string().regex(/^[a-zA-Z0-9._-]{1,32}$/, 'Le riot id doit contenir entre 1 et 32 caractères'),
-  discord: z.string().regex(/^[a-zA-Z0-9._-]{1,32}$/, 'Le discord doit contenir entre 1 et 32 caractères'),
-  rank: z.string().regex(/^[a-zA-Z0-9._ -]{1,32}$/, 'Le rang doit contenir entre 1 et 32 caractères'),
+  username: z.string().regex(/^.{1,32}$/, 'Le nom d\'utilisateur doit contenir entre 1 et 32 caractères'),
+  riot_username: z.string().regex(/^.{1,32}$/, 'Le riot id doit contenir entre 1 et 32 caractères'),
+  discord: z.string().regex(/^.{1,32}$/, 'Le discord doit contenir entre 1 et 32 caractères'),
+  rank: z.string().regex(/^.{1,32}$/, 'Le rang doit contenir entre 1 et 32 caractères'),
   twitch: z.string().optional(),
   fk_team: z.string().optional(),
 });
@@ -12,10 +12,10 @@ export const addPlayerSchema = z.object({
 export type AddPlayerFormData = z.infer<typeof addPlayerSchema>;
 
 export const editPlayerSchema = z.object({
-  username: z.string().regex(/^[a-zA-Z0-9._\s-]{1,32}$/, 'Le nom d\'utilisateur doit contenir entre 1 et 32 caractères'),
-  riot_username: z.string().regex(/^[a-zA-Z0-9._-]{1,32}$/, 'Le riot id doit contenir entre 1 et 32 caractères'),
-  discord: z.string().regex(/^[a-zA-Z0-9._\s-]{1,32}$/, 'Le discord doit contenir entre 1 et 32 caractères'),
-  rank: z.string().regex(/^[a-zA-Z0-9._ -]{1,32}$/, 'Le rang doit contenir entre 1 et 32 caractères'),
+  username: z.string().regex(/^.{1,32}$/, 'Le nom d\'utilisateur doit contenir entre 1 et 32 caractères'),
+  riot_username: z.string().regex(/^.{1,32}$/, 'Le riot id doit contenir entre 1 et 32 caractères'),
+  discord: z.string().regex(/^.{1,32}$/, 'Le discord doit contenir entre 1 et 32 caractères'),
+  rank: z.string().regex(/^.{1,32}$/, 'Le rang doit contenir entre 1 et 32 caractères'),
   twitch: z.string().optional(),
   fk_team: z.string().optional()
 });
